ea8f64ca9e789eae0649711a9ce790263ee6dacd
Benedict Howard <hongha137200110@gmail.com>
2021-08-11 18:26:50 +0700

    database config

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ var firebaseAPI = require('./firebase/firebase');
 (async function myFunction(){
     await firebaseAPI.firestore.collection('cursorEvents').get()
     .then(snapshot => {
-        let events = [];
+        let logs = [];
         snapshot.forEach(doc => {
             const data = doc.data()
             const _id = doc.id
-            events.push({_id, ...data });
+            logs.push({_id, ...data });
         });
-        return events;
+        return logs;
     })
-    .then((events) => {
-        events.sort((a, b) => a.timestamp.localeCompare(b.timestamp));
-        events.splice(0, 1);
+    .then((logs) => {
+        var initialLog = logs.findIndex(element => element.user_id === '0000000000');
+        logs.splice(initialLog, 1);
+        logs.forEach(log => {
+            log.events.sort((a, b) => a.timestamp.localeCompare(b.timestamp));
+            console.log(JSON.stringify(log, null, 4));
+        }); 
         
-        console.log(events);
     })
     .catch((err) => console.log(err));
 })();
